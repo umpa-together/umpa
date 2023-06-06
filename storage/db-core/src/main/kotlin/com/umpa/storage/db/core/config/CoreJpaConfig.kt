a5d71c251f43ce2ac1b87cfd2f29e758f16ce2a6
blackSooooo <blacksooooo@naver.com>
2023-06-06 21:53:12 +0900

    change package and scan base in storage module

--- storage/db-core/src/main/kotlin/com/umpa/storage/db/core/config/CoreJpaConfig.kt
+++ storage/db-core/src/main/kotlin/com/umpa/storage/db/core/config/CoreJpaConfig.kt
@@ -1,6 +1,12 @@
 package com.umpa.storage.db.core.config
 
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @EnableJpaAuditing
+@EntityScan("com.umpa")
+@EnableJpaRepositories("com.umpa")
+@Configuration
 internal class CoreJpaConfig
