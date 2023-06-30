76a1c4598c212c203c4b7a4b496209989fbc7ded
blackSooooo <blacksooooo@naver.com>
2023-07-01 02:11:05 +0900

    @Component 누락 수정

--- umpa-core-api/src/main/kotlin/com/umpa/core/domain/content/playlist/PlaylistUpdater.kt
+++ umpa-core-api/src/main/kotlin/com/umpa/core/domain/content/playlist/PlaylistUpdater.kt
@@ -3,7 +3,9 @@ package com.umpa.core.domain.content.playlist
 import com.umpa.storage.db.core.playlist.PlaylistRepository
 import jakarta.transaction.Transactional
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
 
+@Component
 class PlaylistUpdater(
     private val playlistRepository: PlaylistRepository
 ) {
