9f9d3811a2e175bb23bb98d96655658b6ba8a111
blackSooooo <blacksooooo@naver.com>
2023-07-16 02:51:07 +0900

    resolve test code conflict

--- umpa-core-api/src/test/kotlin/com/umpa/core/domain/auth/JwtTokenIssuerTest.kt
+++ umpa-core-api/src/test/kotlin/com/umpa/core/domain/auth/JwtTokenIssuerTest.kt
@@ -17,7 +17,7 @@ internal class JwtTokenIssuerTest {
 
     @BeforeEach
     fun setup() {
-        key = Keys.hmacShaKeyFor(Decoders.BASE64.decode("testtesttesttesttesttesttesttesttesttesttesttest"))
+        key = Keys.hmacShaKeyFor(Decoders.BASE64.decode("fnafpemfkfofnafpemfkfofnafpemfkfofdsldfknasdlfnlas"))
         sut = JwtTokenHandler(key)
     }
 
@@ -40,7 +40,7 @@ internal class JwtTokenIssuerTest {
     @Test
     fun `토큰을 decode 했을 때 올바른 body를 얻는다`() {
         val actual = sut.decode(
-            "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY4ODE4MTA3MywiZXhwIjoxNjg4MjY3NDczfQ.sQc0TrN73kXwwWUimgDC4vkAUNXsi707pjCWz04KYq0"
+            "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY4OTQ0MzQzMCwiZXhwIjoxNjg5NTI5ODMwfQ.fM2glagAXVAd91TFQcfO-tXgumb8oy_NeVAR3_jHovk"
         )
         val tokenUserId = actual["userId"]
         tokenUserId shouldBe userId
