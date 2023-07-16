1be3e2b024d5b58ec3b12b935b330bb697667602
blackSooooo <blacksooooo@naver.com>
2023-07-16 18:01:10 +0900

    add Daily domain

---
+++ umpa-core-api/src/main/kotlin/com/umpa/core/domain/content/daily/Daily.kt
@@ -0,0 +1,32 @@
+package com.umpa.core.domain.content.daily
+
+import com.umpa.storage.db.core.daily.DailyEntity
+import java.time.LocalDateTime
+
+data class Daily(
+    val id: Long,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+    val userId: Long,
+    val content: String,
+    val imageUrls: List<String>?,
+    val accessedAt: LocalDateTime,
+    val viewCount: Long,
+    val isDeleted: Boolean
+) {
+    companion object {
+        fun fromEntity(entity: DailyEntity): Daily {
+            return Daily(
+                id = entity.id,
+                createdAt = entity.createdAt,
+                updatedAt = entity.updatedAt,
+                userId = entity.userId,
+                content = entity.content,
+                imageUrls = entity.imageUrls,
+                accessedAt = entity.accessedAt,
+                viewCount = entity.viewCount,
+                isDeleted = entity.isDeleted
+            )
+        }
+    }
+}
---
+++ umpa-core-api/src/main/kotlin/com/umpa/core/domain/content/daily/DailyCreation.kt
@@ -0,0 +1,57 @@
+package com.umpa.core.domain.content.daily
+
+import com.umpa.commons.enums.ContentType
+import com.umpa.core.domain.feed.FeedCreation
+import com.umpa.core.domain.hashtag.HashtagCreation
+import com.umpa.core.domain.song.SongCreation
+import com.umpa.core.domain.song.spotify.Track
+import com.umpa.storage.db.core.daily.DailyEntity
+import org.springframework.web.multipart.MultipartFile
+
+data class DailyCreation(
+    val userId: Long,
+    val content: String,
+    val songs: List<Track>,
+    val hashtags: List<String>,
+    val images: List<MultipartFile>?
+) {
+    fun toEntity(): DailyEntity {
+        return DailyEntity(
+            userId = userId,
+            content = content
+        )
+    }
+
+    fun toSongCreations(contentId: Long, contentType: ContentType): List<SongCreation> {
+        return songs.map {
+            SongCreation(
+                userId = userId,
+                contentId = contentId,
+                id = it.id,
+                name = it.name,
+                artistNames = it.artistNames,
+                albumImage = it.albumImage,
+                isExplicit = it.isExplicit,
+                previewUrl = it.previewUrl,
+                contentType = contentType
+            )
+        }
+    }
+
+    fun toFeedCreation(contentId: Long, contentType: ContentType): FeedCreation {
+        return FeedCreation(
+            userId = userId,
+            contentId = contentId,
+            contentType = contentType
+        )
+    }
+
+    fun toHashtagCreations(contentId: Long): List<HashtagCreation> {
+        return hashtags.map {
+            HashtagCreation(
+                hashtag = it,
+                contentId = contentId
+            )
+        }
+    }
+}
